d950699e8fab528f89fbb66ce490743b59024519
safi-999 <cineinformatique@gmail.com>
2025-06-06 22:54:55 +0100

    first commit

---
+++ safi/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+from safi import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('boutique/', include('store.urls')),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
